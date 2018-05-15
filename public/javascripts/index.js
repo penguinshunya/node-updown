d318b2c485474477aa7dec375536f86bae706c3f
penguinshunya <penguinshunya@hotmail.com>
2018-05-15 10:44:10 +0900

    add upload/download system

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,20 @@
-$(() => {
+$(function() {
+  const addFileList = function(file) {
+    $('#files').append(
+      $('<li>').append(
+        $('<a>')
+          .attr('href', `/download/${file.filename}`)
+          .attr('download', file.originalname)
+          .text(file.originalname),
+        $('<button>')
+          .addClass('deleteButton')
+          .attr('type', 'button')
+          .data('filename', file.filename)
+          .text('削除')
+      )
+    );
+  };
+
   $('html').on('drop', function(e) {
     e.stopPropagation();
     e.preventDefault();
@@ -15,13 +31,28 @@ $(() => {
       data: data,
       processData: false,
       contentType: false,
-    }).done(function(text) {
-      console.log(text);
+      dataType: 'json'
+    }).done(function(files) {
+      files.forEach(addFileList);
     });
   });
 
   $('html').on('dragover', function(e) {
     e.stopPropagation();
     e.preventDefault();
-  })
+  });
+
+  $('#files').on('click', '.deleteButton', function(e) {
+    const filename = $(e.target).data('filename');
+
+    $.ajax({
+      url: `/files/${filename}`,
+      type: 'POST',
+      data: {
+        '_method': 'DELETE'
+      }
+    }).done(function() {
+      $(e.target).closest('li').remove();
+    });
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,81 @@
 var express = require('express');
 var multer = require('multer');
+var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
 var router = express.Router();
 var upload = multer({ dest: './uploads/' });
 
+// MONGODB_URL環境変数から、MongoDBのURLを取得する
+const url = `mongodb://${ process.env.MONGODB }`;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    const db = client.db('updown');
+
+    // ファイル情報のリストをPugファイルに渡す
+    db.collection('files').find({}).toArray(function(err, files) {
+      client.close();
+      res.render('index', { title: 'Express', files: files });
+    });
+  });
 });
 
 /* POST upload files. */
 router.post('/upload', upload.array('files'), function(req, res, next) {
-  res.end('finish');
+
+  // MongoDBに接続し、DBにファイル情報を格納する
+  // http://mongodb.github.io/node-mongodb-native/3.0/
+  // { useNewUrlParser: true } を入れないと、DeprecationWarningが表示される
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    const db = client.db('updown');
+
+    db.collection('files').insertMany(req.files.map(function(obj) {
+      return {
+        filename: obj.filename,
+        originalname: obj.originalname,
+        mimetype: obj.mimetype,
+        size: obj.size
+      };
+    }), function(err, result) {
+      client.close();
+
+      res.end(JSON.stringify(req.files.map(function(obj) {
+        return { filename: obj.filename, originalname: obj.originalname };
+      })));
+    });
+  });
+});
+
+/* GET download file. */
+router.get('/download/:filename', function(req, res, next) {
+  const filename = req.params.filename;
+
+  fs.readFile(`./uploads/${filename}`, function(err, data) {
+    res.send(data);
+  });
+});
+
+/* DELETE delete file. */
+/* TODO: どうにかして、router.delete(...) という感じで実装したい */
+router.post('/files/:filename', function(req, res, next) {
+  const filename = req.params.filename;
+
+  switch (req.body._method) {
+  case 'DELETE':
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+      const db = client.db('updown');
+
+      // DB上のファイル情報を削除してから、ファイル本体を削除する
+      db.collection('files').deleteOne({ filename: filename }, function(err, result) {
+        fs.unlink(`./uploads/${filename}`, function(err) {
+          client.close();
+          res.end('delete');
+        });
+      });
+    });
+    break;
+  }
 });
 
 module.exports = router;
