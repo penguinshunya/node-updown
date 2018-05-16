4bc4e5d742773542891948d62ba403f998cef13b
penguinshunya <penguinshunya@hotmail.com>
2018-05-16 15:11:38 +0900

    show progress bar

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,10 @@
 angular.module('updownApp', [])
   .controller('UpdownController', ['$scope', '$http', function($scope, $http) {
     $http.get('/files/').then(function(data) {
-      $scope.files = data.data;
+      $scope.files = data.data.map(function(obj) {
+        obj.showprogress = false;
+        return obj;
+      });
     });
 
     $('html').on('dragover', function(e) {
@@ -18,10 +21,23 @@ angular.module('updownApp', [])
         const data = new FormData();
         data.append('file', file);
 
+        const obj = {
+          originalname: file.name,
+          progress: 0,
+          showprogress: true,
+        };
+        $scope.files.push(obj);
+
         $http.post('/upload', data, {
-          headers: { 'Content-type': undefined }
+          headers: { 'Content-type': undefined },
+          uploadEventHandlers: {
+            progress: function(e) {
+              obj.progress = e.loaded / e.total * 100;
+            }
+          }
         }).then(function(data) {
-          $scope.files.push(data.data);
+          obj.filename = data.data.filename;
+          obj.showprogress = false;
         });
       });
     });
